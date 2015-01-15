9d2e80db90178ccdc07aeec00756557dbc9ebd08
daniw <dani.winz@bluewin.ch>
2015-01-15 07:30:53 +0100

    Init function with check of bitfield orientation

--- driver/l6480.c
+++ driver/l6480.c
@@ -13,3 +13,18 @@
 
 #include "l6480.h"
 
+void l6480_init(void) {
+    /*! test orientation of bitfields */
+    test_bitfield_t test_bitfield;      /*!< testvariable */
+    test_bitfield.bitfield.low = 0x01;  /*!< write testvalue to lower nibble */
+    if (test_bitfield.byte != 0x01) {   /*!< test if lower nibble has been written */
+        while (1) {                     /*!< loop to stop executing if wrong order */
+            /* Hey Programmer */
+            /* It seems that your compiler uses a */
+            /* different order for bitfields than mine. */
+            /* If you still want to use this library, */
+            /* change ,if possible, the order of bitfields in */
+            /* your compiler or in this library! */
+        }
+    }
+}
