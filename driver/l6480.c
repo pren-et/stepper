6270596f43beb5d86594a03145dcbe51162a5089
daniw <dani.winz@bluewin.ch>
2015-01-29 08:36:07 +0100

    Wrong correction from 2db87c39664359f752d3dda044e754aa3f7bae21 corrected

--- driver/l6480.c
+++ driver/l6480.c
@@ -40,11 +40,11 @@ void l6480_send_cmd(uint8_t cmd, uint8_t len, uint8_t read, uint8_t *data) {
     uint8_t i;          /*!< variable to count number of sent bits */
     spi_write(&cmd);    /*!< send command */
     if (read) {         /*!< check if reading data is needed */
-        for (i = 0; i < len; i++) {
+        for (i = 0; i < (len - 1); i++) {
             spi_read(data++);   /*!< read data */
         }
     } else {
-        for (i = 0; i < len; i++) {
+        for (i = 0; i < (len - 1); i++) {
             spi_write(data++);  /*!< write data */
         }
     }
